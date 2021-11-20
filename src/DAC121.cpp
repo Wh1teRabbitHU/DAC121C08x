cc4f98a3d78cc0c515b820b9e73b3af88850cfb1
Tamás Ruszka <ruszka.tamas@gmail.com>
2021-11-20 18:20:52 +0100

    Fixing code by using the proper I2C library

--- src/DAC121.cpp
+++ src/DAC121.cpp
@@ -1,23 +1,27 @@
 #include <DAC121.h>
 
+void DAC121_init() {
+	Wire.begin();
+}
+
 void DAC121_initRegister(uint8_t i2cAddr, uint8_t mode, uint16_t data) {
 	uint16_t maskedMode = (mode << 12) & PDM_MASK;
 	uint16_t maskedData = data & DATA_MASK;
 
-	I2C_8Bit_writeToModule(i2cAddr, maskedData | maskedMode);
+	I2C_16Bit_writeToModule(i2cAddr, maskedData | maskedMode);
 }
 
 void DAC121_setPowerDownMode(uint8_t i2cAddr, uint8_t mode) {
-	uint16_t registerVal = I2C_8Bit_readFromModule(i2cAddr);
+	uint16_t registerVal = I2C_16Bit_readFromModule(i2cAddr);
 
-	registerVal = I2C_8Bit_setBinary(registerVal, PDM_BIT_1_POS, GET_BIT_VALUE(mode, 1));
-	registerVal = I2C_8Bit_setBinary(registerVal, PDM_BIT_0_POS, GET_BIT_VALUE(mode, 0));
+	registerVal = I2C_16Bit_setBinary(registerVal, PDM_BIT_1_POS, GET_BIT_VALUE(mode, 1));
+	registerVal = I2C_16Bit_setBinary(registerVal, PDM_BIT_0_POS, GET_BIT_VALUE(mode, 0));
 
-	I2C_8Bit_writeToModule(i2cAddr, registerVal);
+	I2C_16Bit_writeToModule(i2cAddr, registerVal);
 }
 
 uint8_t DAC121_getPowerDownMode(uint8_t i2cAddr) {
-	uint16_t registerVal = I2C_8Bit_readFromModule(i2cAddr);
+	uint16_t registerVal = I2C_16Bit_readFromModule(i2cAddr);
 
 	return GET_BIT_VALUE(registerVal, PDM_BIT_1_POS) << 1 | GET_BIT_VALUE(registerVal, PDM_BIT_0_POS);
 }
@@ -26,9 +30,9 @@ void DAC121_setData(uint8_t i2cAddr, uint16_t data) {
 	uint16_t maskedData = data & DATA_MASK;
 	uint16_t registerVal = DAC121_getData(i2cAddr) & PDM_MASK;
 
-	I2C_8Bit_writeToModule(i2cAddr, maskedData | registerVal);
+	I2C_16Bit_writeToModule(i2cAddr, maskedData | registerVal);
 }
 
 uint16_t DAC121_getData(uint8_t i2cAddr) {
-	return I2C_8Bit_readFromModule(i2cAddr) & DATA_MASK;
+	return I2C_16Bit_readFromModule(i2cAddr) & DATA_MASK;
 }
--- src/DAC121.h
+++ src/DAC121.h
@@ -1,7 +1,7 @@
 #ifndef DAC121
 #define DAC121
 
-#include <I2C_8Bit.h>
+#include <I2C_16Bit.h>
 
 #define DATA_MASK       0b111111111111
 #define PDM_MASK		0b11 << 12
@@ -13,6 +13,7 @@
 #define PDM_100K_GND	0b10
 #define PDM_HIGH_IMP	0b11
 
+void DAC121_init();
 void DAC121_initRegister(uint8_t i2cAddr, uint8_t mode, uint16_t data);
 
 void DAC121_setPowerDownMode(uint8_t i2cAddr, uint8_t mode);
